2c487af7146681134c890cbc83ec218ff3579cf8
Haziel Ortiz <ya-yo22124@outlook.com>
2024-03-10 15:18:16 -0600

    Upd: Hello World

--- main.py
+++ main.py
@@ -1,14 +1,7 @@
 from fastapi import FastAPI
 
-# Crear una instancia de la aplicación FastAPI
 app = FastAPI()
 
-# Definir una ruta y su operación asociada
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
-
-# Definir otra ruta y operación
-@app.get("/items/{item_id}")
-def read_item(item_id: int, query_param: str = None):
-    return {"item_id": item_id, "query_param": query_param}
+    return {"msg": "Hello World"}
